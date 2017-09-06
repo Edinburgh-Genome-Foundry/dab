8a94ac865d4ce7bbfa41b40fedd86ce12baa7d5b
Zulko <valentin.zulkower@gmail.com>
2017-09-06 11:00:32 +0100

    Started backend linking

--- frontend/src/components/scenarios/DesignConstructs.vue
+++ frontend/src/components/scenarios/DesignConstructs.vue
@@ -3,39 +3,7 @@ div
   h1  {{ infos.title }}
   //- img.icon.center-block(slot='title-img', :src='infos.icon')
   sequencedesigner
-  //-
-  //- .form
-  //-   h4.formlabel What do you want to compute ?
-  //-   el-select(v-model='form.goal', placeholder='Select')
-  //-     el-option(v-for='item in goal_options', :label='item.label',
-  //-               :value='item.value', :key='item.value')
-  //-
-  //-   p.inline Differences between overhangs:
-  //-     el-input-number.inline(v-model="form.overhangs_differences", size="small",
-  //-                            :min=1, :max=4)
-  //-     helper(help='Number of basepairs by which every overhang pair should differ (this includes reverse complements).')
-  //-
-  //-     p.inline GC(%) of the overhangs {{ form.gc_content[0] }} to {{ form.gc_content[1] }}:
-  //-       el-slider(range show-stops v-if="" v-model='form.gc_content',
-  //-                      :max=100, :min=0, :step=25)
-  //-
-  //-   h4.formlabel Forbidden overhangs
-  //-   el-select(v-model='form.forbidden_overhangs',
-  //-             placeholder='Enter any forbidden overhangs e.g. ATGC, GCTA, ...',
-  //-             filterable, :multiple='true')
-  //-     el-option(v-for='overhang in overhangs', :label='overhang', :value='overhang', :key='overhang')
-  //-
-  //-   div(v-if="form.goal === 'overhangs_collection'")
-  //-     h4.formlabel Mandatory overhangs
-  //-     el-select(v-model='form.mandatory_overhangs', placeholder='Enter any mandatory overhangs e.g. ATGC, GCTA, ...',
-  //-               filterable, :multiple='true')
-  //-       el-option(v-for='overhang in overhangs', :label='overhang', :value='overhang', :key='overhang')
-  //-
-  //-   div(v-if="form_goal === 'sequence_decomposition'")
-  //-     sequencesuploader(v-model='form.sequence', text="Drop a single file (or click to select)", :multiple='false')
-  //-   backend-querier(:form='form', :backendUrl='infos.backendUrl',
-  //-                   :validateForm='validateForm', submitButtonText='Evaluate',
-  //-                   v-model='queryStatus')
+  el-button.center(type='primary', :disabled='emptySlots') {{ emptySlots ? 'Some slots are empty' : 'Get sequence(s)' }}
 </template>
 
 <script>
@@ -71,6 +39,11 @@ export default {
           value: 'sequence_decomposition'
         }
       ]
+    }
+  },
+  computed: {
+    emptySlots: function () {
+      return true
     }
   },
   components: {
