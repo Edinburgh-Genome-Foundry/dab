151e1d7361f210d5b1f7dc6b3f55825af62afafd
Aitor Bleda <ableda@staffmail.ed.ac.uk>
2017-10-02 09:44:25 +0100

    Added instructions in the about page

--- frontend/src/components/pages/About.vue
+++ frontend/src/components/pages/About.vue
@@ -1,5 +1,60 @@
 <template lang='pug'>
 .about-emma
+  h1  How to Use EMMA
+
+  h2 Design contructs
+
+  p.
+    The designer allows to design a new construct using EMMA's 25 slot template, along with a set of
+    on/off options like selection markers, homology arms,the transcription units
+    to be used,  etc to suit your EMMA design needs.
+  .example
+          img(src='../../assets/images/Designer_template.png')
+  p.
+    You can delete all the slots except start/stops, to further customize the
+    template, and example of a simple construct would be:
+  .example
+          img(src='../../assets/images/designer_template_reduced.png')
+  p.
+    To create a design, just click on each slot, you will get a popup window and select the part
+    that you want to put there from emmadb's library.
+  .example
+          img(src='../../assets/images/show_selected.png')
+  p.
+    From this part selection, you can click on "ICE Page" (if you want to stay
+    on EMMA designer right click open in new window) to view the part on
+    ICE, genbank to download the genbank file and + to select the part.
+  p Once selected the name of the part will be displayed in the template.
+  p.
+    As soon as you have selected at least one part for each slot, you will be able
+    to click on the bottom "Get design sequence(s)" and you will get a zip file
+    with all the constructs that your selection will generate.
+
+  p.
+    If you have your own EMMA compatuble parts, you can create those in the
+    associated ICE website. This is only possible if you have an account for emmadb.
+    All parts in ICE should be of the type "part"
+  p 1. Click on “Create Entry” and select the type of entry that you are going to create:
+  .example
+        img(src='../../assets/images/create_part.png')
+
+  p 2. Fill the required fields with the data, including Sequence file:
+  p.
+    3. Add two fields: Emma Module with slot number and Category, if in doubt, use CDS
+    You can also browse the
+  p 4. Click submit, and you will go back to the entry main page
+  p.
+    Go back to designer and refresh: you will see your part.
+    Note that by default all created parts are private: you have to share them in
+    ICE with the users you want to see them or with everyone to make them public.
+  p.
+    You can save and upload the schema to work on at a later date.
+
+
+
+
+
+
   h1  About EMMA
 
   h2 Overhangs
@@ -65,6 +120,22 @@ export default {
       margin-right: 1em;
       font-family: 'Inconsolata';
     }
+  }
+  .example img {
+      width:90%;
+      display: block;
+      margin: auto;
+      box-shadow: 2px 2px 2px 2px #ddd;
+  }
+  .examplesmall img {
+      width:40%;
+      display: block;
+      margin: auto;
+  }
+  .exampletiny img {
+      width:20%;
+      display: block;
+      margin: auto;
   }
 
 }
